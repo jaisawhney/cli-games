d1fcb231daaa53b3b0c8882e5efa1a1a9b3b146a
Jai Sawhney <jai.sawhney1@gmail.com>
2021-09-02 12:01:38 -0700

    Flashcards update

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -2,14 +2,31 @@ import json
 import random
 
 
+# Gets the flashcard file
 def get_flashcard_file(file_path):
     with open(file_path, "r") as file:
         file_json = json.load(file)
         return file_json
 
 
-file_json = get_flashcard_file("me-capitals.json")
-cards = file_json["cards"]
+# Asks for the answer to the flashcard
+def ask_question(card):
+    question = card['q']
+    answer = card['a']
+
+    guess = input(f"{question} ")
+
+    if guess.lower() == answer.lower():
+        print(f"Correct! Your score is now {score}/{total}\n")
+        return True
+    print(f"Incorrect! The correct answer was {answer}\n")
+    return False
+
+
+flashcard_json = get_flashcard_file("me-capitals.json")
+cards = flashcard_json["cards"]
+
+# Shuffle the cards
 random.shuffle(cards)
 
 total = len(cards)
@@ -18,15 +35,20 @@ attempts = 0
 
 while score != total:
     for card in cards:
-        guess = input(f"{card['q']} ")
-
         attempts += 1
 
-        if guess.lower() == card["a"].lower():
+        guessed_right = ask_question(card)
+        if guessed_right:
             score += 1
             cards.remove(card)
-            print(f"Correct! Your score is now {score}/{total}\n")
-        else:
-            print(f"Incorrect! The correct answer was {card['a']}\n")
-print("You've completed the flashcards!")
 
+# End message
+print(f"It took you {attempts} attempts to complete the flashcards.")
+
+# Send a message based on the user's attempts
+if attempts == total:
+    print("Flawless!")
+elif attempts >= round(total * 1.5):
+    print("You need more practice!")
+else:
+    print("Good job!")
