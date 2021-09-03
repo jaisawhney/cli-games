33c26c43a32d9b8b779728a377ca45114fac074f
Jai Sawhney <jai.sawhney1@gmail.com>
2021-09-02 17:47:28 -0700

    Add comments

--- asciiart.py
+++ asciiart.py
@@ -5,12 +5,14 @@ text = input("ASCII Art Text: ")
 font = input("ASCII Art Font: ")
 
 
+# Get ascii text
 def get_ascii_art(text, font):
     req = requests.get(f'http://artii.herokuapp.com/make?text={text}{("&font=%s" % font) if font else ""}')
     print("Font:", font)
     print(req.text)
 
 
+# Get all available fonts
 def get_fonts():
     fonts = requests.get('http://artii.herokuapp.com/fonts_list')
     fonts_array = fonts.text.split('\n')
@@ -19,7 +21,6 @@ def get_fonts():
 
 fonts_list = get_fonts()
 if font.lower() == "random":
-    random_font = random.choice(fonts_list)
     for i in range(3):
         font = random.choice(fonts_list)
         get_ascii_art(text, font)
